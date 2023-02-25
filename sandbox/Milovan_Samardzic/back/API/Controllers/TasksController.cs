26e1d5f015eaa1addfeec790c0482302eab2d6f4
Milovan Samardzic <kokaroka9@gmail.com>
2023-02-25 04:38:11 +0100

    Dodat back ispis svih taskova

---
+++ sandbox/Milovan_Samardzic/back/API/Controllers/TasksController.cs
@@ -0,0 +1,22 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController:ControllerBase
+{
+    private readonly TaskDbContext _taskDbContext;
+    public TasksController(TaskDbContext taskDbContext)
+    {
+        _taskDbContext = taskDbContext;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllTasks()
+    {
+        return Ok(await _taskDbContext.Tasks.ToListAsync());
+    }
+}
---
+++ sandbox/Milovan_Samardzic/back/API/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Task
+{
+    [Key]
+    public Guid TaskID { get; set; }
+    public String TaskUser{ get; set; }
+    public String TaskTitle{ get; set; }
+    public String TaskDescription{ get; set; }
+    
+}
