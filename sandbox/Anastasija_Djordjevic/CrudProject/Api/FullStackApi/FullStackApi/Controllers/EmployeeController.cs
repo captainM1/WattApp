d736a65782586dc62d480f85f044463f27d950d0
anastasija.djordjevic <anastasija.dj19@gmail.com>
2023-03-02 02:32:00 +0100

    napravila konekciju sa bazom

---
+++ sandbox/Anastasija_Djordjevic/CrudProject/Api/FullStackApi/FullStackApi/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStackApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FullStackApi.Models;
+
+namespace FullStackApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbcontext;
+
+        public EmployeeController(FullStackDbContext fullStackDbcontext)
+        {
+            _fullStackDbcontext = fullStackDbcontext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _fullStackDbcontext.employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            await _fullStackDbcontext.employees.AddAsync(employee);
+            await _fullStackDbcontext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
--- sandbox/Anastasija_Djordjevic/CrudProject/FullStackUI/FullStackUI/src/app/components/employees/employees-list/employees-list.component.ts
+++ sandbox/Anastasija_Djordjevic/CrudProject/FullStackUI/FullStackUI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.css']
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit{
+
+  employees: Employee[] = [
+    {
+      id: '5b4ed4cc-f316-444b-a06e-05ce7b322892',
+      name: 'John Doe',
+      email: 'john.doe@email.com',
+      phone: 998877665,
+      salary: 60000,
+      department: 'Human Resources'
+    },
+    {
+      id: '5b4ed4cc-c316-444b-a06e-05ce73456892',
+      name: 'Sameer Saini',
+      email: 'sameer.saini@email.com',
+      phone: 880987665,
+      salary: 150000,
+      department: 'Information technologies'
+    }
+  ];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
