b44b916e9ddf33678041bc7d76d8e9727a38ef14
kataa00 <kzdravkovic1@gmail.com>
2023-03-09 14:13:35 +0100

    Dodata logika za tokene

--- src/appProsumer/prosumerAppUI/src/app/services/auth.service.ts
+++ src/appProsumer/prosumerAppUI/src/app/services/auth.service.ts
@@ -9,6 +9,32 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
+  private baseUrl: string = "https://localhost:7234/api/User/";
   constructor(private http: HttpClient) { }
+
+  signUp(userObj:any)
+  {
+    return this.http.post<any>(`${this.baseUrl}register`, userObj);
+  }
+
+  login(loginObj:any)
+  {
+    return this.http.post<any>(`${this.baseUrl}authenticate`, loginObj);
+  }
+
+  storeToken(tokenValue:string)
+  {
+    localStorage.setItem('token', tokenValue);
+  }
+
+  getToken()
+  {
+    return localStorage.getItem('token');
+  }
+
+  isloggedIn():boolean
+  {
+    return !!localStorage.getItem('token');
+  }
 
 }
