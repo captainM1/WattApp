433eea7e0908b78ed3e8a1004ca084653fe71df9
kataa00 <kzdravkovic1@gmail.com>
2023-03-10 23:55:06 +0100

    Promenjena registracija i login da budu Observable

--- src/appProsumer/prosumerAppUI/src/app/services/auth.service.ts
+++ src/appProsumer/prosumerAppUI/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 
@@ -9,18 +11,26 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private baseUrl: string = "https://localhost:7234/api/User/";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
 
-  signUp(userObj:any)
-  {
-    return this.http.post<any>(`${this.baseUrl}register`, userObj);
+  login(email : string, password : string) : Observable<string>{
+    return this.http.post<string>(environment.apiUrl + "/authenticate", {
+      email : email,
+      password : password
+    })
   }
 
-  login(loginObj:any)
-  {
-    return this.http.post<any>(`${this.baseUrl}authenticate`, loginObj);
+  register(username: string, email : string,  address: string, number : string, password : string) : Observable<string>{
+    return this.http.post<string>(environment.apiUrl + "/register", {
+      username : username,
+      email : email,
+      address : address,
+      number : number,
+      password : password
+    })
   }
+
+
 
   storeToken(tokenValue:string)
   {
