4cd699b5b3671dea3f4ffc047c933b503f9a6923
anastasija.djordjevic <anastasija.dj19@gmail.com>
2023-03-11 20:11:04 +0100

    login and registration endpoint

---
+++ sandbox/Anastasija_Djordjevic/project1/project1/Data/DatabContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using project1.Models;
+
+namespace project1.Data
+
+{
+    public class DatabContext : DbContext
+    {
+        public DatabContext(DbContextOptions<DatabContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost;database=Users;Encrypt=False;TrustServerCertificate=False;User ID=SA;Password={password};");
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
+
---
+++ sandbox/Anastasija_Djordjevic/project1/project1/Models/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace project1.Models
+{
+	public class Response
+	{
+		public int statusCode { get; set; }
+		public string statusMessage { get; set; } = string.Empty;
+	}
+}
+
