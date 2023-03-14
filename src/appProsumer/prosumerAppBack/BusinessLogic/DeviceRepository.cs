eb932d6850a4f8432d7c0539cce55edf115511a7
anastasija.djordjevic <anastasija.dj19@gmail.com>
2023-03-14 15:43:13 +0100

    endpoints for getting one and all devices

--- src/appProsumer/prosumerAppBack/BusinessLogic/DeviceRepository.cs
+++ src/appProsumer/prosumerAppBack/BusinessLogic/DeviceRepository.cs
@@ -18,12 +18,22 @@ namespace prosumerAppBack.BusinessLogic
         public async Task<Device> GetDeviceByIdAsync(Guid id)
         {
             var device = await _mongoDataContext.Devices.Find(d => d.ID == id).FirstOrDefaultAsync();
+            if(device == null)
+            {
+                return null;
+            }
+
             return device;
         }
 
         public async Task<List<Device>> GetAllDevices()
         {
             var devices = await _mongoDataContext.Devices.Find(_ => true).ToListAsync();
+            if(devices == null)
+            {
+                return null;
+            }
+
             return devices;
         }
     }
