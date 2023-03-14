28b729f2f12c1cf78ca9b09c2c4ed2d0cfdda7f8
anastasija.djordjevic <anastasija.dj19@gmail.com>
2023-03-14 10:58:39 +0100

    Device model

---
+++ src/appProsumer/prosumerAppBack/Models/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace prosumerAppBack.Models
+{
+	public class Device
+	{
+		public Guid ID { get; set; }
+		public string? Name { get; set; }
+		public string? Manufacurer { get; set; }
+		public double Wattage { get; set; }
+		public double UsageFrequency { get; set; }
+		public string? MacAdress { get; set; }
+		public TimeSpan UsageTime { get; set; }
+		public int DeviceAge { get; set; }
+    }
+}
+
--- src/appProsumer/prosumerAppBack/Models/User.cs
+++ src/appProsumer/prosumerAppBack/Models/User.cs
@@ -12,7 +12,7 @@ namespace prosumerAppBack.Models
         public byte[] PasswordHash { get; set; }
         public byte[] Salt { get; set; }
         
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         
         public string? Address { get; set; }
         public string? City { get; set; }
