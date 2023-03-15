20063ce5b23c1c115bd5b0cc7e9262d129634c60
emilija.ristic <114-2018@pmf.kg.ac.rs>
2023-03-15 16:30:10 +0100

    #32 dodat popup message za login i signup

--- src/appProsumer/prosumerAppUI/src/app/app.module.ts
+++ src/appProsumer/prosumerAppUI/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import {NgToastModule} from 'ng-angular-popup'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -32,7 +32,8 @@ import { HomeComponent } from './components/home/home.component';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    NgToastModule
 
   ],
   providers: [],
