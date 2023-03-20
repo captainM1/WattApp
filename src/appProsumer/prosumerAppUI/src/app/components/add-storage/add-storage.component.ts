abd81cc0e38b33dde84cfcf388d56cb63769c103
olivera-obretkovic <oliveraobretkovic.inf@gmail.com>
2023-03-20 21:37:44 +0100

    AddStorage forma i stil

--- src/appProsumer/prosumerAppUI/src/app/components/add-storage/add-storage.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/add-storage/add-storage.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 @Component({
   selector: 'app-add-storage',
   templateUrl: './add-storage.component.html',
   styleUrls: ['./add-storage.component.css']
 })
 export class AddStorageComponent {
+  submitted = false;
+  addStorageForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder, 
+    private router : Router,
+    private toast : NgToastService
+  ){}
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.addStorageForm.valid){
+      this.toast.success({detail:"Success", summary: "Adding device successful!", duration:3000});
+      this.router.navigate(['home']);
+      return;
+    }else{
+      this.toast.error({detail:"Error", summary:"Something went wrong!", duration:3000 })
+      this.router.navigate(['addDevice'])
+    }
+
+  }
 }
