8e3b21d6a433b7459321dd64727af15e61ca118e
olivera-obretkovic <oliveraobretkovic.inf@gmail.com>
2023-03-21 21:20:35 +0100

    Forma i stil za addProducer

--- src/appProsumer/prosumerAppUI/src/app/components/add-producer/add-producer.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/add-producer/add-producer.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 @Component({
   selector: 'app-add-producer',
   templateUrl: './add-producer.component.html',
   styleUrls: ['./add-producer.component.css']
 })
 export class AddProducerComponent {
+  submitted = false;
+  addProducerForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder, 
+    private router : Router,
+    private toast : NgToastService
+  ){}
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.addProducerForm.valid){
+      this.toast.success({detail:"Success", summary: "Adding device successful!", duration:3000});
+      this.router.navigate(['home']);
+      return;
+    }else{
+      this.toast.error({detail:"Error", summary:"Something went wrong!", duration:3000 })
+      this.router.navigate(['addDevice'])
+    }
+
+  }
 }
