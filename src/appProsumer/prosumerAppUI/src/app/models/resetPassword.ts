daa90d283565eee268094269277ef19438b55f85
emilija.ristic <114-2018@pmf.kg.ac.rs>
2023-03-21 17:12:56 +0100

    #28 forgot pass

--- src/appProsumer/prosumerAppUI/src/app/models/resetPassword.ts
+++ src/appProsumer/prosumerAppUI/src/app/models/resetPassword.ts
@@ -1,7 +1,4 @@
 export class ResetPassword{
-    sendResetPasswordLink(resetPasswordEmail: string) {
-      throw new Error('Method not implemented.');
-    }
     public email!: string;
     public emailToken!: string;
     public newPassword!: string;
--- src/appProsumer/prosumerAppUI/src/app/services/forgot-password.service.ts
+++ src/appProsumer/prosumerAppUI/src/app/services/forgot-password.service.ts
@@ -8,16 +8,21 @@ import { ResetPassword } from '../models/resetPassword';
 })
 export class ForgotPasswordService {
 
-  private baseUrl: string = environment.apiUrl + "";
+  private baseUrl: string = environment.apiUrl;
   constructor(
     private http: HttpClient
   ) { }
 
   sendResetPasswordLink(email : string){
-    return this.http.post<any>(this.baseUrl+"/../"+email,{});
+    return this.http.post<any>(this.baseUrl+'api/User/send-email',email,{});
   }
 
   resetPassword(resetPasswordObj : ResetPassword){
-    return this.http.post<any>(this.baseUrl+"/..", resetPasswordObj)
+    return this.http.post<any>(this.baseUrl+ 'api/User/reset-password' ,resetPasswordObj)
   }
+
+  newPassword(newPassword : string, token : string){
+    return this.http.put(environment.apiUrl+ 'api/User/new-password', {token, newPassword});
+  }
+
 }
