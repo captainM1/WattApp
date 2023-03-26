ed684b84c4b329a8de9077cc0b65e79d532bbf9d
kataa00 <kzdravkovic1@gmail.com>
2023-03-26 13:47:45 +0200

    Izmena css

--- src/appProsumer/prosumerAppUI/src/app/components/my-devices/my-devices.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/my-devices/my-devices.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-devices',
   templateUrl: './my-devices.component.html',
-  styleUrls: ['./my-devices.component.css'],
-  template: `
-    <div [style.width]="40%" [style.height]="70%">
-      <app-my-devices></app-my-devices>
-    </div>
-  `,
+  styleUrls: ['./my-devices.component.css']
 })
 export class MyDevicesComponent {
 
