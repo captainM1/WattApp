0bb9d5702e21f3120c4a5264d5d05c98b26a396c
kataa00 <kzdravkovic1@gmail.com>
2023-03-26 12:20:10 +0200

    Izmenjen css i dodata responzivnost

--- src/appProsumer/prosumerAppUI/src/app/components/my-devices/my-devices.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/my-devices/my-devices.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-devices',
   templateUrl: './my-devices.component.html',
-  styleUrls: ['./my-devices.component.css']
+  styleUrls: ['./my-devices.component.css'],
+  template: `
+    <div [style.width]="40%" [style.height]="70%">
+      <app-my-devices></app-my-devices>
+    </div>
+  `,
 })
 export class MyDevicesComponent {
 
