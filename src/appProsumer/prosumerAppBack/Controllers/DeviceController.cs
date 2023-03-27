4dd0ffc538e898e536a915845f82fea22cb28dfa
Milovan Samardzic <kokaroka9@gmail.com>
2023-03-28 01:37:46 +0200

    Konflikti2

--- src/appProsumer/prosumerAppBack/Controllers/DeviceController.cs
+++ src/appProsumer/prosumerAppBack/Controllers/DeviceController.cs
@@ -54,18 +54,17 @@ namespace prosumerAppBack.Controllers
         }
     }
 
-        [HttpPost("devices/add-new")]
-        public async Task<IActionResult> AddDevice([FromBody] Models.Device.AddDeviceDto addDeviceDto)
-        {
-            var check = await _deviceRepository.AddDevice(addDeviceDto);
+	[HttpPost("devices/add-new")]
+	public async Task<IActionResult> AddDevice([FromBody] Models.Device.AddDeviceDto addDeviceDto)
+	{
+		var check = await _deviceRepository.AddDevice(addDeviceDto);
 
-            if (check)
-            {
-                return BadRequest("Cannot add device");
-            }
+		if (check)
+		{
+			return BadRequest("Cannot add device");
+		}
 
-            return Ok(new { message = "Device added" });
-        }
-    }
+		return Ok(new { message = "Device added" });
+	}
 }
 
