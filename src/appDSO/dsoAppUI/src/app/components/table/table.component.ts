129364fe113f0b476c71f8a6422052314e41d53e
emilija.ristic <114-2018@pmf.kg.ac.rs>
2023-03-30 13:01:48 +0200

    #68 dodata mapa, auth guard

--- src/appDSO/dsoAppUI/src/app/components/table/table.component.ts
+++ src/appDSO/dsoAppUI/src/app/components/table/table.component.ts
@@ -1,27 +1,92 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { AuthService } from 'service/auth.service';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
-  animations: [
-     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ]
+  
 })
-export class TableComponent {
-
+export class TableComponent implements OnInit{
+  
+  userCoords!: any[];
+  coords?: number[];
+  userIDCoords! : any[];
+  allUsers!: any[];
+  
+  private map!: L.Map;
+  private centroid: L.LatLngExpression = [44.0128,20.9114]; 
+  private marker! : L.Marker;
   
   
   constructor(
-    private http : HttpClient
+    private auth : AuthService
   ){}
 
+
+  ngOnInit(): void {
+    this.auth.getPagination(1,5).subscribe(
+      (response : any)=> {
+        this.allUsers = response;
+      }
+    );
+
+    this.auth.getUserCoords().subscribe(
+      (response : any) =>{
+        this.userCoords = response;
+        console.log(this.userCoords[0].address);
+        this.coords = JSON.parse(response[0].coordinates);
+        console.log(this.coords)
+        
+        
+       
+      });
+
+    this.auth.getCoordsID("5E12058A-2F83-4F95-B108-BF0B5811DAEE").subscribe(
+      (response : any) =>{
+        
+        console.log("USER",response);
+      }
+    )
+    this.initMap();
+  }
+  
+  private initMap(): void{
+    this.map = L.map('map',{
+      center: this.centroid,
+      zoom: 12
+  });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?key=Xjmy4FwEg0zjH3KOEzXy',{
+      maxZoom:18,
+      minZoom: 10,
+      
+    });
+
+    
+
+    // const popup = L.popup().setLatLng(this.centroid).setContent('<p>User coords:<br> Total producing: <br> Total consuping: <br></p>').openOn(this.map);
+    var marker = L.marker([44.0128,20.9114],
+      {alt: 'Kyiv'}).addTo(this.map) // "Kyiv" is the accessible name of this marker
+      .bindPopup('<p>First name: <br>Last Name: <br>Total Production: <br> Total Consumption:</p>');
+    // this.marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+  
+    // const jittery = Array(5).fill(this.centroid).map(
+    //   x =>[x[0]+(Math.random()- .5)/10, x[1]+(Math.random() - .5)/10],
+      
+    // ).map(
+    //   x=>L.marker(x as L.LatLngExpression)
+    // ).forEach(
+    //   x => x.addTo(this.map)
+    // );
+      tiles.addTo(this.map);
+
+      // tiles.bindPopup(popup).openPopup();
+  } 
+  
   
 
 }
---
+++ src/appDSO/dsoAppUI/src/service/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'app/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUserCoords():Observable<any>{
+    return this.http.get<any>(environment.apiUrl + "/api/User/coordinatesForEveryUser");
+  }
+
+  getPagination(pageNumber : number, pageSize : number) : Observable<any>{
+    return this.http.get<any>(environment.apiUrl + "/api/User/users", {
+      params: {
+        pageSize : pageSize,
+        pageNumber : pageNumber
+      }
+    })
+  }
+
+  getCoordsID(id : string):Observable<any>{
+    return this.http.get<any>(environment.apiUrl + "/api/User/coordinates/"+id);
+  }
+}
