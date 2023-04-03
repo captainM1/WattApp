0cd1ad7ed8b5e1f81d287ddc280f2996c30467c1
olivera-obretkovic <oliveraobretkovic.inf@gmail.com>
2023-04-03 21:18:42 +0200

    Uzimanje id device-a

--- src/appProsumer/prosumerAppUI/src/app/components/device-details/device-details.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/device-details/device-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-device-details',
@@ -11,13 +13,20 @@ export class DeviceDetailsComponent implements OnInit {
   device: any;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpClient
   )
   {}
 
   ngOnInit() {
     const deviceId = this.route.snapshot.paramMap.get('id');
     console.log(deviceId);
+
+    this.http.get<any[]>(`${environment.apiUrl}/api/Device/devices/${deviceId}`)
+      .subscribe(data => {
+        this.device = data;
+      });
+    console.log(this.device);
   }
 
 }
