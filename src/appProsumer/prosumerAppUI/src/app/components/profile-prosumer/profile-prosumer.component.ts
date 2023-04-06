09457a054017967dd32216c4424226edafb7213e
emilija.ristic <114-2018@pmf.kg.ac.rs>
2023-04-06 12:48:50 +0200

    #80 token, dekodiranje tokena, dodati podaci na profile prosumer

--- src/appProsumer/prosumerAppUI/src/app/components/profile-prosumer/profile-prosumer.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/profile-prosumer/profile-prosumer.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
+
+import jwt_decode from 'jwt-decode';
+import { AuthUserService } from 'src/app/services/auth-user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile-prosumer',
   templateUrl: './profile-prosumer.component.html',
   styleUrls: ['./profile-prosumer.component.css']
 })
-export class ProfileProsumerComponent {
+export class ProfileProsumerComponent implements OnInit{
+  
+  allUsers!: any[];
+  userID!: User;
+  token!:any;
+ 
 
+
+  constructor(
+    private auth : AuthUserService,
+    private serv : AuthService
+  ){}
+
+  ngOnInit(): void {
+    this.getUser();
+    this.getToken();
+    
+  }
+
+  getUser(){
+    this.auth.getAllUsers().subscribe(
+      (response : any)=>{
+        console.log(response);
+      }
+    )
+  }
+
+  getToken(){
+    this.token = this.serv.getToken();
+    console.log(this.token)
+    this.auth.getThisUser(this.token).subscribe(
+      (response :any)=>{
+       this.userID = response;
+       console.log(this.userID);
+      }
+    )
+  }
+
+ 
+ 
 }
+
+
+
+
--- src/appProsumer/prosumerAppUI/src/app/services/auth-user.service.ts
+++ src/appProsumer/prosumerAppUI/src/app/services/auth-user.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthUserService {
-
   
-  constructor(private http : HttpClient) { 
-    
+  
+  constructor(
+    private http : HttpClient,
+    private auth : AuthService,
+    private cookie : CookieService) {}
+
+  getUserByID(userID : string) : Observable<any>{
+    return this.http.get<any>(environment.apiUrl + '/api/User/users/' + userID);
   }
 
-  // getUser(){
-  //   return this.http.get<any>(environment.apiUrl);
-  // }
+  getAllUsers() : Observable<any>{
+    return this.http.get<any>(environment.apiUrl + '/api/User/allUserInfo');
+  }
 
+  getThisUser(userID:string) : Observable<any>{
+    return this.http.get<any>(`${environment.apiUrl}/api/User/users/${userID}`, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
+  }
+
+  
 }
