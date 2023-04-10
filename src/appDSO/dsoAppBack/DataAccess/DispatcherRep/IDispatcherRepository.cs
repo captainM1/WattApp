962085e93a00eb0e4a69f847a03134cc4796a3e3
darko.djurdjevic <darkodjurdjevic9@gmail.com>
2023-04-10 11:24:24 +0200

    odradjena login i registracija za DSO

---
+++ src/appDSO/dsoAppBack/DataAccess/DispatcherRep/IDispatcherRepository.cs
@@ -0,0 +1,13 @@
+﻿using dsoAppBack.Models;
+
+namespace dsoAppBack.DataAccess.DispatcherRep;
+
+public interface IDispatcherRepository
+{
+    Task<Dispatcher> GetDispatcherByIdAsync(Guid id);
+    Task<Dispatcher> GetUserByEmailAndPasswordAsync(string email, string password);
+    Task<Boolean> UpdatePassword(Guid id, string newPassword);
+    Task<Dispatcher> CreateDispatcher(DispatcherRegisterDto dispatcherRegisterDto);
+    Task<Dispatcher> GetDispatcherByEmailAsync(string email);
+    Task<Dispatcher> GetDispatcherByUsernameAsync(string username);
+}
