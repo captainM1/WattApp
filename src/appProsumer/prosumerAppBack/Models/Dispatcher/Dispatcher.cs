fb1e348835d0efdd192f5e688b5a2c6eb731cbf2
darko.djurdjevic <darkodjurdjevic9@gmail.com>
2023-04-12 09:41:48 +0200

    odradjenja registracija i login za dso

---
+++ src/appProsumer/prosumerAppBack/Models/Dispatcher/Dispatcher.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace prosumerAppBack.Models.Dispatcher
+{
+    public class Dispatcher
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public string? UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
+        public string? Role { get; set; }
+        public string? Email { get; set; }
+    }
+}
