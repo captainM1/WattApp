73b2ae9a8084d996c70180070f66bfbc6c61c09f
darko.djurdjevic <darkodjurdjevic9@gmail.com>
2023-04-12 09:50:56 +0200

    dodato prihvatanje i odbijanje zahteva korsinika za deljenje podataka sa dso

--- src/appProsumer/prosumerAppBack/DataAccess/UserRep/UserRepository.cs
+++ src/appProsumer/prosumerAppBack/DataAccess/UserRep/UserRepository.cs
@@ -67,7 +67,7 @@ public class UserRepository : IUserRepository
             Country = userRegisterDto.Address.Split(",")[2],
             Salt = salt,
             PasswordHash = hash,
-            Role = "RegularUser",
+            Role = "UnapprovedUser",
             ID = Guid.NewGuid(),
         };
         _dbContext.Users.Add(newUser);
@@ -187,6 +187,8 @@ public class UserRepository : IUserRepository
     {
         var newUser = new UsersRequestedToDso
         {
+            FirstName = user.FirstName,
+            LastName = user.LastName,
             UserName = user.UserName,
             PhoneNumber = user.PhoneNumber,
             Email = user.Email,
@@ -201,6 +203,46 @@ public class UserRepository : IUserRepository
         await _dbContext.SaveChangesAsync();
         return true;
     }
+
+    public async Task<Boolean> ApproveUserRequestToDso(Guid id)
+    {
+        var newUser = await _dbContext.UsersAppliedToDSO.FindAsync(id);
+
+        var approvedUser = new User
+        {
+            ID = newUser.ID,
+            FirstName = newUser.FirstName,
+            LastName = newUser.LastName,
+            UserName = newUser.UserName,
+            PhoneNumber = newUser.PhoneNumber,
+            Email = newUser.Email,
+            Address = newUser.Address,
+            City = newUser.City,
+            Country = newUser.Country,
+            Salt = newUser.Salt,
+            PasswordHash = newUser.PasswordHash,
+            Role = "RegularUser",
+        };
+
+        _dbContext.Users.Update(approvedUser);
+        await _dbContext.SaveChangesAsync();
+
+        var user = await _dbContext.UsersAppliedToDSO.FindAsync(id);
+
+        _dbContext.UsersAppliedToDSO.Remove(user);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Boolean> DeclineUserRequestToDso(Guid id)
+    {
+        var user = await _dbContext.UsersAppliedToDSO.FindAsync(id);
+
+        _dbContext.UsersAppliedToDSO.Remove(user);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+
     public async Task<int> GetNumberOfUsers()
     {
         return await _dbContext.Users.CountAsync();
