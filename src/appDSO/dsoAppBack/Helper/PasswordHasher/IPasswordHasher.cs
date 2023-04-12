89186fea735037643004914708dcc6a6f1d6ccfb
Milovan Samardzic <kokaroka9@gmail.com>
2023-04-12 20:34:10 +0200

    Obrisan drugi back

--- src/appDSO/dsoAppBack/Helper/PasswordHasher/IPasswordHasher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace dsoAppBack.Helper.PasswordHasher;
-
-public interface IPasswordHasher
-{
-    (byte[] salt, byte[] hash) HashPassword(string password);
-    bool VerifyPassword(string password, byte[] salt, byte[] hash);
-}
--- src/appDSO/dsoAppBack/Helper/TokenMaker/ITokenMaker.cs
+++
@@ -1,9 +0,0 @@
-﻿using dsoAppBack.Models;
-
-namespace dsoAppBack.Helper.TokenMaker;
-
-public interface ITokenMaker
-{
-    string GenerateToken(Dispatcher dispatcher);
-    bool ValidateJwtToken(string token);
-}
