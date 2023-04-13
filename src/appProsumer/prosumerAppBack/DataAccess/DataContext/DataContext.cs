0471e7c01f71925ff0a27e82f2183ebc27b402ff
Milovan Samardzic <kokaroka9@gmail.com>
2023-04-13 06:42:20 +0200

    README

--- src/appProsumer/prosumerAppBack/DataAccess/DataContext/DataContext.cs
+++ src/appProsumer/prosumerAppBack/DataAccess/DataContext/DataContext.cs
@@ -46,6 +46,57 @@ namespace prosumerAppBack.DataAccess
                 .HasOne(d => d.Owner)
                 .WithMany(u => u.Devices)
                 .HasForeignKey(d => d.OwnerID);
+            modelBuilder.Entity<DeviceGroup>().HasData(
+                new DeviceGroup{ID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"),Name = "Consumer"},
+                new DeviceGroup{ID = new Guid("18F30035-59DE-474F-B9DB-987476DE551F"),Name = "Producer"},
+                new DeviceGroup{ID = new Guid("B17C9155-7E6F-4D37-8A86-EA1ABB327BB2"),Name = "Storage"}
+            );
+            modelBuilder.Entity<DeviceManufacturers>().HasData(
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4D"), Name = "Bosch"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4E"), Name = "Siemens"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4F"), Name = "Miele"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D50"), Name = "AEG"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D51"), Name = "Electrolux"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D52"), Name = "Zanussi"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D53"), Name = "Beko"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D54"), Name = "Indesit"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D55"), Name = "Candy"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D56"), Name = "Whirlpool"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D57"), Name = "Gorenje"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D58"), Name = "Smeg"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D59"), Name = "Hoover"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D60"), Name = "Grundig"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D61"),Name = "Neff"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D62"),Name = "Bauknecht"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D63"),Name = "Hotpoint"},
+                new DeviceManufacturers{ID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D64"),Name = "Ariston"}
+            );
+
+            modelBuilder.Entity<DeviceType>().HasData(
+                new DeviceType{ID=new Guid("9D3D39B2-56D8-44E7-8AD5-B64EFC6784FE"), Name = "Washing Machine", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D61"), Wattage = 1000},
+
+            new DeviceType{ID=new Guid("D3105304-6EC5-4AED-9B53-9C7EF8E81C4C"), Name = "Oven", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D60"), Wattage = 3500},
+
+            new DeviceType{ID=new Guid("73E8B43E-BFAF-4DB9-9F36-CF40CC057A6C"), Name = "Refrigerator", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D58"), Wattage = 250},
+
+            new DeviceType{ID=new Guid("815F9D3E-F0F8-4E0D-9B6E-9043293BEE9D"), Name = "Dishwasher", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D52"), Wattage = 1800},
+
+            new DeviceType{ID=new Guid("1AC7203E-B15C-47CE-BC23-08B5B62D225E"), Name = "Dryer", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4E"), Wattage = 3400},
+
+            new DeviceType{ID=new Guid("0BDA9B57-DF0E-485E-B209-409B26F046E0"), Name = "Cooker Hood", GroupID = new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID = new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D61"), Wattage = 110},
+                new DeviceType{ID=new Guid("06BAAAD5-80B8-446B-9480-948E8BA9D52B"), Name="Microwave", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D63"), Wattage=1100},
+
+            new DeviceType{ID=new Guid("1435A6E0-FE87-4B65-90F2-CAB08ABC51FC"), Name="Freezer", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D52"), Wattage=200},
+
+            new DeviceType{ID=new Guid("DA04E45D-559B-4B24-B20B-2D7335DB2CF0"), Name="Range", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D56"), Wattage=4500},
+
+            new DeviceType{ID=new Guid("311175CE-F67C-4F5B-B96C-A11243534F3F"), Name="Washing Machine", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4D"), Wattage=1000},
+
+            new DeviceType{ID=new Guid("783D8BD7-725B-42B6-A76B-6E9AD0FCA6DA"), Name="Refrigerator", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D51"), Wattage=250},
+
+            new DeviceType{ID=new Guid("32EA7105-F582-4441-AE81-B738C4284F7E"), Name="Dishwasher", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D54"), Wattage=1800},
+                new DeviceType{ID=new Guid("F2F9BE26-5C5F-43E1-AA2F-8E64960D03DD"), Name="Dryer", GroupID=new Guid("77CBC929-1CF2-4750-900A-164DE4ABE28B"), ManufacturerID=new Guid("4D4D4D4D-4D4D-4D4D-4D4D-4D4D4D4D4D4D"), Wattage=3400}
+            );
         }
     }
 }
