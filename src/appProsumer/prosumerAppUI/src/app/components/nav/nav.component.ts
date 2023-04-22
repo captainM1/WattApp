ee3de2f441c37fc95d9d6826ad0bed54e389e92b
olivera-obretkovic <oliveraobretkovic.inf@gmail.com>
2023-04-22 20:55:44 +0200

    Dodata potvrda za logout desktop verzija

--- src/appProsumer/prosumerAppUI/src/app/components/nav/nav.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { ConfirmationService, MessageService, ConfirmEventType } from 'primeng/api';
 
 @Component({
   selector: 'app-nav',
@@ -12,10 +13,32 @@ export class NavComponent {
 
   constructor(
     private auth: AuthService,
-    private router : Router){}
+    private router : Router,
+    private confirmationService : ConfirmationService,
+    private messageService : MessageService){}
 
   signOut(){
     this.auth.signOut();
     this.router.navigate(['signin']);
+  }
+  confirmSignOut(){
+    this.confirmationService.confirm({
+      message: 'Do you want to log out?',
+      header: 'Log out',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.signOut();
+      },
+      reject: (type: any) => {
+        switch (type) {
+          case ConfirmEventType.REJECT:
+            this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+            break;
+          case ConfirmEventType.CANCEL:
+            this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'You have cancelled' });
+            break;
+        }
+      }
+    });
   }
 }
