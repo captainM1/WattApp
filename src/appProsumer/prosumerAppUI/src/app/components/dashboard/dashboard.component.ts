7e4a9b7d3d270e4c7dd6730c8c5c555a49d98a5e
kataa00 <kzdravkovic1@gmail.com>
2023-04-30 16:37:17 +0200

    Dodat grafik za consumption previous 7 days i sredjeno sortiranje datuma

--- src/appProsumer/prosumerAppUI/src/app/components/dashboard/dashboard.component.ts
+++ src/appProsumer/prosumerAppUI/src/app/components/dashboard/dashboard.component.ts
@@ -19,7 +19,10 @@ export class DashboardComponent implements OnInit, AfterViewInit{
   extractedDatesNextMonth!:string[];
   consumptionPrevMonthUser!:[];
   consumptionNextMonthUser!:[];
-
+  consPrev7Days = [];
+  timeStrampConsumptionPrev7days = [];
+  powerUsageConsumptionPrev7days = [];
+  extractedDatesPrev7Days!:string[];
 
   constructor(
 		private auth : AuthService,
@@ -28,7 +31,7 @@ export class DashboardComponent implements OnInit, AfterViewInit{
 
   @ViewChild('consumptionPrevMonthGraph') consumptionPrevMonthGraph!:ElementRef;
   @ViewChild('consumptionNextMonthGraph') consumptionNextMonthGraph!:ElementRef;
-
+  @ViewChild('consumptionPrev7daysGraph') consumptionPrev7daysGraph!:ElementRef;
 
   ngAfterViewInit(): void {
   }
@@ -47,6 +50,7 @@ export class DashboardComponent implements OnInit, AfterViewInit{
        console.log(this.userID);
        this.consumptionNextMonth(this.userID);
        this.consumptionPrevMonth(this.userID);
+       this.consumptionPrev7Days(this.userID);
       }
     )
   }
@@ -103,6 +107,9 @@ export class DashboardComponent implements OnInit, AfterViewInit{
 
     }
 
+    this.extractedDatesPrevMonth.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+
     const data = {
       labels: this.extractedDatesPrevMonth,
       datasets: [{
@@ -158,6 +165,7 @@ export class DashboardComponent implements OnInit, AfterViewInit{
 
     }
 
+    this.extractedDatesNextMonth.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
 
     const data = {
       labels: this.extractedDatesNextMonth,
@@ -205,6 +213,60 @@ export class DashboardComponent implements OnInit, AfterViewInit{
     });
   }
 
+  consumptionPrev7Days(id : any){
+    this.auth1.getConsumptionPrev7days(id).subscribe({
+      next:(response : any) => {
+        this.consPrev7Days = response[0]['timestampPowerPairs'];
+        this.makeDataGraphPrev7DaysConsumption(this.consPrev7Days);
+        console.log(this.consPrev7Days);
+        this.chartConsumptionPrev7Days();
+      },
+      error : (err : any) => {
+        console.log("error consumptio previous 7 days");
+      }
+    })
+  }
+
+
+  makeDataGraphPrev7DaysConsumption(dataGraph : any){
+    for(let i = 0; i < dataGraph.length; i++){
+      this.timeStrampConsumptionPrev7days.push(this.consPrev7Days[i]['timestamp']);
+      this.powerUsageConsumptionPrev7days.push(this.consPrev7Days[i]['powerUsage']);
+    }
+  }
+
+  chartConsumptionPrev7Days(){
+    for(let i = 0; i < this.timeStrampConsumptionPrev7days.length; i++){
+      const dateStringList = this.timeStrampConsumptionPrev7days.toString();
+      const substrings = dateStringList.split(',');
+     this.extractedDatesPrev7Days = substrings.map(date => date.substring(0, date.indexOf('T')));
+
+    }
+    console.log(this.extractedDatesPrev7Days);
+    console.log(this.powerUsageConsumptionPrev7days);
+
+    this.extractedDatesPrev7Days.sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+    const data = {
+      labels: this.extractedDatesPrev7Days,
+      datasets: [{
+        label: 'Previous 7 days Consumption',
+        data: this.powerUsageConsumptionPrev7days,
+        fill: true,
+        borderColor: 'rgb(59, 193, 74)',
+					backgroundColor:'rgba(59, 193, 74,0.4)',
+					pointBackgroundColor: 'rgba(59, 193, 74,0.7)',
+					borderWidth: 1,
+					pointBorderColor:'rgb(59, 193, 74)'
+
+      }]
+    }
+
+      const stackedLine = new Chart(this.consumptionPrev7daysGraph.nativeElement, {
+        type: 'bar',
+        data: data,
+
+      });
+  }
 
 
 }
--- src/appProsumer/prosumerAppUI/src/app/services/auth-user.service.ts
+++ src/appProsumer/prosumerAppUI/src/app/services/auth-user.service.ts
@@ -49,7 +49,7 @@ export class AuthUserService {
   }
 
   getConsumptionPrevious24Hours(userID:any) :Observable<any>{
-    return this.http.get<any>(environment.apiUrl + "/api/PowerUsage/power-usage/previous24Hours/consumption/user-every-day-device-usage/"+userID,{ headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
+    return this.http.get<any>(environment.apiUrl + "/api/PowerUsage/power-usage/previous24Hours/consumption/user-every-day-device-usage/"+userID, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
   }
 
   getDeviceInfoUserByID(userID : any){
@@ -57,9 +57,14 @@ export class AuthUserService {
   }
 
   getConsumptionPrevMonth(userID : any) : Observable<any>{
-    return this.http.get(environment.apiUrl + "/api/PowerUsage/power-usage/previousMonth/consumption/user-every-day-device-usage/"+userID,{ headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
+    return this.http.get(environment.apiUrl + "/api/PowerUsage/power-usage/previousMonth/consumption/user-every-day-device-usage/"+userID, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
   }
+
   getConsumptionNextMonth(userID: any):Observable<any>{
-    return this.http.get(environment.apiUrl + "/api/PowerUsage/power-usage/nextMonth/consumption/user-every-day-device-usage/" + userID,{ headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
+    return this.http.get(environment.apiUrl + "/api/PowerUsage/power-usage/nextMonth/consumption/user-every-day-device-usage/" + userID, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
+  }
+
+  getConsumptionPrev7days(userID : string):Observable<any>{
+    return this.http.get(environment.apiUrl + "/api/PowerUsage/power-usage/previous7Days/consumption/user-every-day-device-usage/" + userID, { headers: new HttpHeaders().set('Authorization', `Bearer ${this.cookie.get('jwtToken')}`) });
   }
 }
